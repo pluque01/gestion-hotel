78b3ab29aa28fa263c7a4dd89563c50edcc2d887
Jaime Martinez <jamartbr@gmail.com>
2025-01-18 09:51:05 +0100

    añadido protección de datos

--- src/main/java/ddsi/ademat/Main.java
+++ src/main/java/ddsi/ademat/Main.java
@@ -16,6 +16,11 @@ public class Main {
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Conexión establecida con éxito a la base de datos");
 
+
+            // Informar al usuario sobre el tratamiento de sus datos y sus derechos
+            System.out.println("Sus datos personales serán tratados conforme al Reglamento General de Protección de Datos (RGPD) y la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos Personales y garantía de los derechos digitales (LOPDGDD).");
+            System.out.println("Puede consustar sus derechos en la Agencia Española de Protección de Datos (www.aepd.es).");
+
             boolean exit = false;
             Scanner scanner = new Scanner(System.in);
 
